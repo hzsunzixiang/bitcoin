2328fe12cc320d31bd41f9b587f648995225fd6d
hzsunzixiang <haichengsun123@163.com>
2018-08-05 08:25:48 +0800

    bitcoin

--- study/tx/key.sh
+++ study/tx/key.sh
@@ -20,13 +20,13 @@ StephenSun@debian-1:~/bitcoin/study/tx$ rt getaddressinfo 2MsvmB4K5yFMxAdFhGyGW8
   "isscript": true,
   "iswitness": false,
   "script": "witness_v0_keyhash",
-  "hex": "0014682f951f473c437f4489af026e5bfb1d1ed22aa3",
+  "hex": "0014682f951f473c437f4489af026e5bfb1d1ed22aa3",  # (0x0014{20-byte-key-hash}) 0x0014 加上对公钥的hash
   "pubkey": "02b9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf",
   "embedded": {
     "isscript": false,
     "iswitness": true,
     "witness_version": 0,
-    "witness_program": "682f951f473c437f4489af026e5bfb1d1ed22aa3",
+    "witness_program": "682f951f473c437f4489af026e5bfb1d1ed22aa3",  # 这里是公钥的hash
     "pubkey": "02b9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf",
     "address": "bcrt1qdqhe286883ph73yf4upxuklmr50dy24ra7qpd0",
     "scriptPubKey": "0014682f951f473c437f4489af026e5bfb1d1ed22aa3"
@@ -117,10 +117,11 @@ ffffffff # Then we write a four-byte field denoting the sequence. This is curren
 02 # Next comes a one-byte varint containing the number of outputs in our new transaction.  2 in this example: 02 
 4023178000000000  # We then write an 8-byte field (64 bit integer, little-endian) containing the amount we want to redeem from the specified output   2149000000   0x80172340
 17 # Then we start writing our transaction's output. We start with a one-byte varint denoting the length of the output script (0x17 or 23 bytes): 17 
-a914077a414c3d707eaff2718369bad42b26878279c887  # 对应"scriptPubKey"
+a914077a414c3d707eaff2718369bad42b26878279c887  # 对应输出的 "scriptPubKey"
+                                                #意思是"OP_HASH160 077a414c3d707eaff2718369bad42b26878279c8 OP_EQUAL",
 808cdfa900000000
 17
-a91462983ea52b359d304548bf09e4a09f4a4ac7b70087
+a91462983ea52b359d304548bf09e4a09f4a4ac7b70087 # 另外一个
 00000000 # Then we write the four-byte "lock time" field: 00000000
 
 
@@ -194,19 +195,19 @@ rt descendantcount "02000000000101ffad836d201185414f5d94b2ed0145e594c5d9f66e696c
 ffad836d201185414f5d94b2ed0145e594c5d9f66e696c8aefe2d8e1c4e5cd36 # 32字节的hash逆序
 00000000  #Four-byte field denoting the output index we want to redeem from the transaction with the above hash (counting from zero): 00000000
 17  #Now comes the scriptSig. For the purpose of signing the transaction, this is temporarily filled with the scriptPubKey of the output we want to redeem. First we write a one-byte varint which denotes the length of the scriptSig (0x17 = 23 bytes): 17
-16
-0014682f951f473c437f4489af026e5bfb1d1ed22aa3 # 对应上面的 "hex": "0014682f951f473c437f4489af026e5bfb1d1ed22aa3",
+16  # 16在这里是长度 0x16=22
+0014682f951f473c437f4489af026e5bfb1d1ed22aa3 # 对应上面的 "hex": "0014" + pubkey的hash值,
 ffffffff #Then we write a four-byte field denoting the sequence. This is currently always set to 0xffffffff: ffffffff
 02 # Next comes a one-byte varint containing the number of outputs in our new transaction. set this to 2 in this example: 02
 4023178000000000  # 8-byte field (64 bit integer, little-endian) containing the amount 808cdfa900000000
 17  # 0x17  = 23 
 a914077a414c3d707eaff2718369bad42b26878279c887 
 808cdfa900000000
-17 # 0x17
+17 # 0x17 = 23
 a91462983ea52b359d304548bf09e4a09f4a4ac7b70087
-02  # TODO 从这里开始设计 txinwitness 不明白这里的含义 可能就是签名，需要重新审视这里
+02  # 从这里开始涉及 txinwitness  这里的02 代表 TODO   2个字段? 版本 ?
 48   # 0x48 = 72
-3045022100cacee8914755740fc1d347ba2188b8f951daf5f9a5f6f1ed2d47a015cffead37022066511fefcd546494bc8d068cc879a70b62c8e2745bf272ea071eaa1085ab879e01   # 我想这个应该是签名
+3045022100cacee8914755740fc1d347ba2188b8f951daf5f9a5f6f1ed2d47a015cffead37022066511fefcd546494bc8d068cc879a70b62c8e2745bf272ea071eaa1085ab879e01   # 签名 我猜测这个签名和之前的区别是：之前是把签名放在中间，现在放在结尾
 21   # 0x21 = 33
 02b9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf   # 这个是公钥 
 00000000
@@ -222,7 +223,7 @@ a91462983ea52b359d304548bf09e4a09f4a4ac7b70087
       "txid": "36cde5c4e1d8e2ef8a6c696ef6d9c594e54501edb2945d4f418511206d83adff",
       "vout": 0,
       "scriptSig": {
-        "asm": "0014682f951f473c437f4489af026e5bfb1d1ed22aa3",
+        "asm": "0014682f951f473c437f4489af026e5bfb1d1ed22aa3", # 0x0014 + 对公钥的hash
         "hex": "160014682f951f473c437f4489af026e5bfb1d1ed22aa3"
       },
       "txinwitness": [
@@ -261,3 +262,33 @@ a91462983ea52b359d304548bf09e4a09f4a4ac7b70087
     }
   ]
 }
+
+从下面这个类型可以看出
+        "type": "scripthash",
+这其实是一个支付到脚本hash(P2SH)
+
+
+printf  "02b9c7077daaa55acf00048bca3c5d04d053a5a4e48c32c88e6776ccc275c94daf" | xxd -r -p |sha256sum -b |xxd -r -p |openssl rmd160 
+
+---------------------------------------------------------------------------------
+P2WPKH
+
+The following example is a version 0 pay-to-witness-public-key-hash (P2WPKH):
+
+    witness:      <signature> <pubkey>
+    scriptSig:    (empty)
+    scriptPubKey: 0 <20-byte-key-hash>    # 这里的keyhash是对公钥的hash
+                  (0x0014{20-byte-key-hash})
+
+The '0' in scriptPubKey indicates the following push is a version 0 witness program. The length of the witness program indicates that it is a P2WPKH type. The witness must consist of exactly 2 items. The HASH160 of the pubkey in witness must match the witness program.
+
+The signature is verified as
+
+    <signature> <pubkey> CHECKSIG
+
+Comparing with a traditional P2PKH output, the P2WPKH equivalent occupies 3 less bytes in the scriptPubKey, and moves the signature and public key from scriptSig to witness. 
+
+---------------------------------------------------------------------------------
+        
+
+
